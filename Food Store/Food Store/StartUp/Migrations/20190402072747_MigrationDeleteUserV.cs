85cfefc339e3d8a54df9c0e8376da6f9bf8c486b
VladimirRadev <neymaradev@gmail.com>
2019-04-02 10:32:32 +0300

    see changelog

---
+++ Food Store/Food Store/StartUp/Migrations/20190402072747_MigrationDeleteUserV.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StartUp.Migrations
+{
+    public partial class MigrationDeleteUserV : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    MoneyToShop = table.Column<double>(nullable: false),
+                    UserName = table.Column<string>(maxLength: 25, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+    }
+}
